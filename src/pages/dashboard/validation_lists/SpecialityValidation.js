c82db651aec638828283f4c32757a904013d890e
vmaxmahesh <107479074+vmaxmahesh@users.noreply.github.com>
2022-11-11 16:22:26 +0530

    Speciality Validation List displayed and form data also

--- src/pages/dashboard/validation_lists/SpecialityValidation.js
+++ src/pages/dashboard/validation_lists/SpecialityValidation.js
@@ -1,7 +1,132 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { render } from 'react-dom';
+import { useForm } from 'react-hook-form';
+import { Link, Outlet, useLocation, useOutletContext } from 'react-router-dom';
 
 export default function SpecialityValidation()
 {
+
+    const scollToRef = useRef();
+
+
+    const [ndcData, setNdcData] = useState([]);
+    const [ndcClass, setNdClass] = useState([]);
+
+    const [selctedNdc, setSelctedNdc] = useState('');
+
+
+
+    const searchException = (fdata) => {
+        
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        };
+
+        fetch(process.env.REACT_APP_API_BASEURL + `/api/validationlist/speciality/search?search=${fdata.target.value}`, requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+                //  console.log(response);
+                // console.log(data.data);
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    setNdcData([]);
+                    return Promise.reject(error);
+
+                } else {
+                    setNdcData(data.data);
+                    return;
+                }
+
+
+
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }
+    const getNDCItems = (ndcid) => {
+        // ndc_exception_list
+        var test = {};
+        test.ndc_exception_list = ndcid;
+        setSelctedNdc(test);
+
+        // //  console.log(customerid);
+        const requestOptions = {
+            method: 'GET',
+            // mode: 'no-cors',
+            headers: { 'Content-Type': 'application/json' },
+            // body: encodeURIComponent(data)
+        };
+        // //  console.log(watch(fdata));
+
+        fetch(process.env.REACT_APP_API_BASEURL + `/api/validationlist/speciality/get/${ndcid}`, requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+                //  console.log(response);
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    setNdClass([]);
+                    return Promise.reject(error);
+                } else {
+                    console.log(data.data);
+                    setNdClass(data.data);
+                    // scollToRef.current.scrollIntoView()
+                }
+
+
+                if (response === '200') {
+                }
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }
+
+    const getNDCItemDetails = (ndcid) => {
+        //  console.log(ndcid);
+        const requestOptions = {
+            method: 'GET',
+            // mode: 'no-cors',
+            headers: { 'Content-Type': 'application/json' },
+            // body: encodeURIComponent(data)
+        };
+        //  console.log(watch(fdata));
+
+        fetch(process.env.REACT_APP_API_BASEURL + `/api/validationlist/speciality/details/${ndcid}`, requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+                //  console.log(response);
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                } else {
+                    setSelctedNdc(data.data);
+                    // scollToRef.current.scrollIntoView()
+                    // return;
+                }
+
+
+                if (response === '200') {
+                }
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }
+
     return(
         <>
          <div className="row">
@@ -24,13 +149,27 @@ export default function SpecialityValidation()
                     </div>
                 </div>
             </div> 
-            <SearchSpeciality />
+            <SearchSpeciality searchException={searchException} />
+
+
+            <SpecialityList ndcListData={ndcData} ndcClassData={ndcClass} getNDCItem={getNDCItems} getNDCItemDetails={getNDCItemDetails} selctedNdc={selctedNdc} />
+
+<SpecialityForm  viewDiagnosisFormdata={selctedNdc} />
+
         </>
     )
 }
 
-function SearchSpeciality()
+function SearchSpeciality(props)
 {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+
+    const searchException = (fdata) => {
+
+        props.searchException(fdata);
+    }
     return(
         <>
          <div className="card mt-3 mb-3">
@@ -39,19 +178,97 @@ function SearchSpeciality()
                         <div className="col-md-12 mb-3">
                             <div className="form-group">
                                 <small>Speciality Validation ID/Name</small>
-                                <input type="text"  className="form-control" placeholder='Start typing speciality validation ID/name to search'
+                                <input type="text"  onKeyUp={(e) => searchException(e)} className="form-control" placeholder='Start typing speciality validation ID/name to search'
                                 />
                             </div>
                         </div>                       
                     </div>
                 </div>
             </div>
-            <SpecialityList />
+
+
         </>
     )
 }
 
-function SpecialityList() {
+
+
+function NdcRow(props) {
+
+    useEffect(() => {
+
+    }, [props.selected]);
+
+
+
+    return (
+        <>
+            <tr className={(props.selected && props.ndcRow.specialty_list == props.selected.specialty_list ? ' tblactiverow ' : '')}
+
+                onClick={() => props.getNDCItem(props.ndcRow.specialty_id)}
+            >
+                <td>{props.ndcRow.specialty_id}</td>
+                <td >{props.ndcRow.exception_name}</td>
+
+                {/* <td><button className="btn btn-sm btn-info" id="" ><i className="fa fa-eye"></i> View</button></td> */}
+            </tr>
+        </>
+    )
+}
+
+
+function NdcClassRow(props) {
+
+    useEffect(() => {
+
+    }, [props.selected]);
+
+    return (
+        <>
+            <tr
+                className={(props.selected && props.ndcClassRow.specialty_id == props.selected.specialty_id ? ' tblactiverow ' : '')}
+                onClick={() => props.getNDCItemDetails(props.ndcClassRow.specialty_id)}
+
+            >
+                <td>{props.ndcClassRow.priority}</td>
+                <td>{props.ndcClassRow.specialty_status}</td>
+              
+                {/* <td><button className="btn btn-sm btn-info" id="" ><i className="fa fa-eye"></i> View</button></td> */}
+            </tr>
+        </>
+    )
+}
+
+function SpecialityList(props) {
+
+
+    const scollToRef = useRef();
+
+    useEffect(() => { }, [props.selctedNdc]);
+    // //  console.log(props.selctedNdc);
+
+    const getNDCItem = (ndciemid) => {
+        props.getNDCItem(ndciemid);
+    }
+
+    const getNDCItemDetails = (ndciemid) => {
+        props.getNDCItemDetails(ndciemid);
+    }
+
+    const ndcListArray = [];
+    for (let i = 0; i < props.ndcListData.length; i++) {
+        ndcListArray.push(<NdcRow ndcRow={props.ndcListData[i]} getNDCItem={getNDCItem} selected={props.selctedNdc} />);
+    }
+
+    const ndcClassArray = [];
+    for (let j = 0; j < props.ndcClassData.length; j++) {
+        ndcClassArray.push(<NdcClassRow ndcClassRow={props.ndcClassData[j]} getNDCItemDetails={getNDCItemDetails} selected={props.selctedNdc} />);
+    }
+
+    const [ncdListData, setNcdListData] = useState();
+    const [show, setShow] = useState("none");
+    const handleShow = () => setShow("block");
+
     return (  
         <>
           <div className="card mt-3 mb-3">
@@ -76,6 +293,8 @@ function SpecialityList() {
                                             </thead>
                                             <tbody>
 
+                                            {ndcListArray}
+
                                             </tbody>
                                         </table>
                                     </div>
@@ -95,6 +314,7 @@ function SpecialityList() {
                                                 </tr>
                                             </thead>
                                             <tbody>
+                                            {ndcClassArray}
                                             </tbody>
                                         </table>
                                     </div>
@@ -104,12 +324,19 @@ function SpecialityList() {
                     </div>
                 </div>
             </div>
-            <SpecialityForm />
+            {/* <SpecialityForm /> */}
         </>
     );
 }
 
-function SpecialityForm() {
+function SpecialityForm(props) {
+
+
+    const { register,reset, handleSubmit, watch, formState: { errors } } = useForm();
+
+    // const [selctedNdc, setSelctedNdc] = useOutletContext();
+
+    useEffect(() => { reset(props.viewDiagnosisFormdata) }, [props.viewDiagnosisFormdata]);
     return (  
         <>
          <div className="card mt-3 mb-3">
@@ -121,20 +348,20 @@ function SpecialityForm() {
                                 <div className="col-md-4 mb-3">
                                     <div className="form-group">
                                         <small> Specialty List ID: </small>
-                                       <input type="text" name="" placeholder="" className="form-control" />
+                                       <input type="text" name="specialty_list" {...register('specialty_list')} placeholder="" className="form-control" />
                                     </div>
                                 </div>
                                 <div className="col-md-4 mb-3">
                                     <div className="form-group">
                                         <small> Specialty List Name: </small>
-                                    <input type="text" name="" placeholder="100PC" className="form-control" />
+                                    <input type="text" name="exception_name" {...register('exception_name')} placeholder="100PC" className="form-control" />
                                     </div>
                                 </div>
                                 <div className="col-md-4">
                                     <div className="form-group ">
                                          <small> Specialty ID: </small>
                                         <div className="searchmodal">
-                                       <input type="text" name="" className="form-control" placeholder="" autoComplete="off" />
+                                       <input type="text" name="specialty_id" {...register('specialty_id')} className="form-control" placeholder="" autoComplete="off" />
                                        <button className="btn-info"><i className="fa-solid fa-magnifying-glass"></i></button>
                                        </div>
                                     </div>
@@ -142,8 +369,9 @@ function SpecialityForm() {
                                  <div className="col-md-4 mb-3">
                                     <div className="form-group">
                                         <small> Specialty Status: </small>
-                                            <select className="form-select">
-                                                <option>Approved</option>
+                                            <select className="form-select" name="specialty_status" {...register('specialty_status')}>
+                                                <option value="">--select--</option>
+                                                <option value="A">Approved</option>
                                             </select>
                                     </div>
                                 </div>
