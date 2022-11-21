dcfad9131fb18a726bfdfa6b441a197e3284d650
vmaxmahesh <107479074+vmaxmahesh@users.noreply.github.com>
2022-11-21 16:53:02 +0530

    GPI Exclusion Listadded

--- src/pages/dashboard/accumulated_benefits/GPIExclusion.js
+++ src/pages/dashboard/accumulated_benefits/GPIExclusion.js
@@ -1,7 +1,128 @@
-import React from 'react';
-
+import React, { useEffect, useRef, useState } from 'react';
+import { render } from 'react-dom';
+import { useForm } from 'react-hook-form';
+import { Link, Outlet, useLocation, useOutletContext } from 'react-router-dom';
 export default function GPIExclusion()
 {
+
+
+    const [ndcData, setNdcData] = useState([]);
+    const [ndcClass, setNdClass] = useState([]);
+
+    const [selctedNdc, setSelctedNdc] = useState('');
+
+
+    const searchException = (fdata) => {
+        
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        };
+
+        fetch(process.env.REACT_APP_API_BASEURL + `/api/validationlist/gpiExclusion/search?search=${fdata.target.value}`, requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+                //  console.log(response);
+                // console.log(data.data);
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    setNdcData([]);
+                    return Promise.reject(error);
+
+                } else {
+                    setNdcData(data.data);
+                    return;
+                }
+
+
+
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }
+    const getNDCItems = (ndcid) => {
+        // ndc_exception_list
+        var test = {};
+        test.ndc_exception_list = ndcid;
+        setSelctedNdc(test);
+
+        // //  console.log(customerid);
+        const requestOptions = {
+            method: 'GET',
+            // mode: 'no-cors',
+            headers: { 'Content-Type': 'application/json' },
+            // body: encodeURIComponent(data)
+        };
+        // //  console.log(watch(fdata));
+
+        fetch(process.env.REACT_APP_API_BASEURL + `/api/validationlist/gpiExclusion/get/${ndcid}`, requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+                //  console.log(response);
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    setNdClass([]);
+                    return Promise.reject(error);
+                } else {
+                    // console.log(data.data);
+                    setNdClass(data.data);
+                    // scollToRef.current.scrollIntoView()
+                }
+
+
+                if (response === '200') {
+                }
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }
+
+    const getNDCItemDetails = (ndcid) => {
+        //  console.log(ndcid);
+        const requestOptions = {
+            method: 'GET',
+            // mode: 'no-cors',
+            headers: { 'Content-Type': 'application/json' },
+            // body: encodeURIComponent(data)
+        };
+        //  console.log(watch(fdata));
+
+        fetch(process.env.REACT_APP_API_BASEURL + `/api/validationlist/gpiExclusion/details/${ndcid}`, requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+                //  console.log(response);
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                } else {
+                    setSelctedNdc(data.data);
+                    // scollToRef.current.scrollIntoView()
+                    // return;
+                }
+
+
+                if (response === '200') {
+                }
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }
+
     return(
         <>
          <div className="row">
@@ -24,13 +145,28 @@ export default function GPIExclusion()
                     </div>
                 </div>
             </div>
-            <SearchGPIExclusion />
+            <SearchGPIExclusion searchException={searchException} />
+
+            <GPIExclusionList ndcListData={ndcData} ndcClassData={ndcClass} getNDCItem={getNDCItems} getNDCItemDetails={getNDCItemDetails} selctedNdc={selctedNdc} />
+
+
+            <GPIExclusionForm  viewDiagnosisFormdata={selctedNdc} />
+
+
         </>
     )
 }
 
-function SearchGPIExclusion()
+function SearchGPIExclusion(props)
 {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+
+    const searchException = (fdata) => {
+
+        props.searchException(fdata);
+    }
     return(
         <>
          <div className="card mt-3 mb-3">
@@ -39,20 +175,100 @@ function SearchGPIExclusion()
                         <div className="col-md-12 mb-3">
                             <div className="form-group">
                                 <small>GPI Exclusion</small>
-                                <input type="text" className="form-control" placeholder='Start typing  GPI exclusion ID/name to search'
+                                <input type="text" className="form-control"  onKeyUp={(e) => searchException(e)} placeholder='Start typing  GPI exclusion ID/name to search'
                                 />
                             </div>
                         </div>                       
                     </div>
                 </div>
             </div> 
-            <GPIExclusionList />
+            {/* <GPIExclusionList /> */}
         </>
     )
 }
 
-function GPIExclusionList()
+
+
+
+function NdcRow(props) {
+
+    useEffect(() => {
+
+    }, [props.selected]);
+
+
+
+    return (
+        <>
+            <tr className={(props.selected && props.ndcRow.gpi_exclusion_list == props.selected.gpi_exclusion_list ? ' tblactiverow ' : '')}
+
+                onClick={() => props.getNDCItem(props.ndcRow.generic_product_id)}
+            >
+                <td>{props.ndcRow.gpi_exclusion_list}</td>
+                <td >{props.ndcRow.exclusion_name}</td>
+
+                {/* <td><button className="btn btn-sm btn-info" id="" ><i className="fa fa-eye"></i> View</button></td> */}
+            </tr>
+        </>
+    )
+}
+
+
+function NdcClassRow(props) {
+
+    useEffect(() => {
+
+    }, [props.selected]);
+
+
+    // console.log(props.selected);
+
+    return (
+        <>
+            <tr
+                className={(props.selected && props.ndcClassRow.generic_product_id == props.selected.generic_product_id ? ' tblactiverow ' : '')}
+                onClick={() => props.getNDCItemDetails(props.ndcClassRow.generic_product_id)}
+
+            >
+                <td>{props.ndcClassRow.generic_product_id}</td>
+              
+                {/* <td><button className="btn btn-sm btn-info" id="" ><i className="fa fa-eye"></i> View</button></td> */}
+            </tr>
+        </>
+    )
+}
+
+function GPIExclusionList(props)
 {
+
+
+    const scollToRef = useRef();
+
+    useEffect(() => { }, [props.selctedNdc]);
+    // //  console.log(props.selctedNdc);
+
+    const getNDCItem = (ndciemid) => {
+        props.getNDCItem(ndciemid);
+    }
+
+    const getNDCItemDetails = (ndciemid) => {
+        props.getNDCItemDetails(ndciemid);
+    }
+
+    const ndcListArray = [];
+    for (let i = 0; i < props.ndcListData.length; i++) {
+        ndcListArray.push(<NdcRow ndcRow={props.ndcListData[i]} getNDCItem={getNDCItem} selected={props.selctedNdc} />);
+    }
+console.log(props.ndcClassData);
+    const ndcClassArray = [];
+    for (let j = 0; j < props.ndcClassData.length; j++) {
+        ndcClassArray.push(<NdcClassRow ndcClassRow={props.ndcClassData[j]} getNDCItemDetails={getNDCItemDetails} selected={props.selctedNdc} />);
+    }
+
+
+    const [ncdListData, setNcdListData] = useState();
+    const [show, setShow] = useState("none");
+    const handleShow = () => setShow("block");
     return(
         <>
          <div className="card mt-3 mb-3">
@@ -77,6 +293,9 @@ function GPIExclusionList()
                                             </thead>
                                             <tbody>
 
+                                            {ndcListArray}
+
+
                                             </tbody>
                                         </table>
                                     </div>
@@ -90,10 +309,12 @@ function GPIExclusionList()
                                         <table className="table table-striped table-bordered" style={{ position: 'relative' }}>
                                             <thead className='stickt-thead'>
                                                 <tr>
-                                                    <th>GPI Exclusion Status</th>
+                                                    <th>GPI </th>
                                                 </tr>
                                             </thead>
                                             <tbody>
+
+                                            {ndcClassArray}
 
                                             </tbody>
                                         </table>
@@ -104,13 +325,19 @@ function GPIExclusionList()
                     </div>
                 </div>
             </div> 
-            <GPIExclusionForm />
+            {/* <GPIExclusionForm /> */}
         </>
     )
 }
 
-function GPIExclusionForm()
+function GPIExclusionForm(props)
 {
+
+    const { register,reset, handleSubmit, watch, formState: { errors } } = useForm();
+
+    // const [selctedNdc, setSelctedNdc] = useOutletContext();
+
+    useEffect(() => { reset(props.viewDiagnosisFormdata) }, [props.viewDiagnosisFormdata]);
     return(
         <>
          <div className="card mt-3 mb-3">
@@ -122,20 +349,20 @@ function GPIExclusionForm()
                                 <div className="col-md-4 mb-3">
                                     <div className="form-group">
                                         <small> List ID</small>
-                                       <input type="text" name="" className="form-control" placeholder="" />
+                                       <input type="text" name="gpi_exclusion_list"  {...register('gpi_exclusion_list')} className="form-control" placeholder="" />
                                     </div>
                                 </div>
                                 <div className="col-md-4 mb-3">
                                     <div className="form-group">
                                         <small> List Name</small>
-                                     <input type="text" name="" className="form-control" placeholder="" />
+                                     <input type="text" name="exclusion_name" {...register('exclusion_name')} className="form-control" placeholder="" />
                                     </div>
                                 </div>
                                 <div className="col-md-4 mb-3">
                                     <div className="form-group ">
                                          <small> GPI</small>
                                         <div className="searchmodal">
-                                       <input type="text" name="" className="form-control" placeholder="" />
+                                       <input type="text" name="generic_product_id" {...register('generic_product_id')} className="form-control" placeholder="" />
                                        {/* <button className="btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal"><i className="fa-solid fa-magnifying-glass"></i></button> */}
                                        </div>
                                     </div>
