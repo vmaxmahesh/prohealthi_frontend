b4512b13e1add7540d93575b9ab5bd3ff812fa6e
vmaxmahesh <107479074+vmaxmahesh@users.noreply.github.com>
2022-12-01 14:40:20 +0530

    copay strategy

--- src/pages/dashboard/strategies/CopayStrategy.js
+++ src/pages/dashboard/strategies/CopayStrategy.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { render } from 'react-dom';
 import { useForm } from 'react-hook-form';
 import { Link, Outlet, useLocation, useOutletContext } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 
 export default function CopayStrategy() {
@@ -14,6 +16,12 @@ export default function CopayStrategy() {
     const [ndcClass, setNdClass] = useState([]);
 
     const [selctedNdc, setSelctedNdc] = useState('');
+
+    const [benifitsData, setBenifitData] = useState(false);
+    const [adding, setAdding] = useState(false);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const searchException = (fdata) => {
 
@@ -93,6 +101,30 @@ export default function CopayStrategy() {
     }
 
 
+    useEffect(() => {
+        if (benifitsData) {
+            setAdding(false);
+
+        } else {
+            setAdding(true);
+            setBenifitData(false);
+        }
+
+        document.title = 'Benefit Code | ProHealthi';
+
+    }, [benifitsData, adding]);
+
+
+    const AddForm = () => {
+        setBenifitData(false);
+        setAdding(true);
+
+        
+
+    }
+
+
+
     const getNDCItemDetails = (ndcid) => {
         console.log(ndcid);
         const requestOptions = {
@@ -115,7 +147,7 @@ export default function CopayStrategy() {
                     const error = (data && data.message) || response.status;
                     return Promise.reject(error);
                 } else {
-                    setSelctedNdc(data.data);
+                    setBenifitData(data.data);
                     console.log(selctedNdc);
                     scollToRef.current.scrollIntoView()
                     return;
@@ -155,13 +187,19 @@ export default function CopayStrategy() {
                 </div>
             </div>
 
+
+            <div className="col-md-3 ms-auto text-end">
+                    <button className="btn  btn-info btn-sm" onClick={e => AddForm()}>
+                    Copay Strategy Strategy <i className="fa fa-plus-circle"></i></button>
+            </div>
+
             <SearchCopayStrategy searchException={searchException} />
 
 
             <CopayStrategyList ndcListData={ndcData} ndcClassData={ndcClass} getNDCItem={getNDCItems} getNDCItemDetails={getNDCItemDetails} selctedNdc={selctedNdc} />
 
-            <CopayStrategyForm viewDiagnosisFormdata={selctedNdc} />
-            <CopayStrategyIdentifiers />
+            <CopayStrategyForm  show={show} adding={adding} handleClose={handleClose} selected={benifitsData} />
+
         </>
     )
 }
@@ -347,10 +385,97 @@ function CopayStrategyForm(props) {
     const { register, reset, handleSubmit, watch, formState: { errors } } = useForm();
 
 
-    useEffect(() => { reset(props.viewDiagnosisFormdata) }, [props.viewDiagnosisFormdata]);
+
+    const addCode = (data) => {
+        // console.log(data);
+        const requestOptions = {
+            method: 'POST',
+            // mode: 'no-cors',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+
+        };
+        // console.log(watch(data)); 
+        if (process.env.REACT_APP_API_BASEURL == 'NOT') {
+            toast.success('Added Successfully...!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+
+            });
+        } else {
+            fetch(process.env.REACT_APP_API_BASEURL + `/api/validationlist/copay/add`, requestOptions)
+                .then(async response => {
+                    const isJson = response.headers.get('content-type')?.includes('application/json');
+                    const data = isJson && await response.json();
+                    // console.log(response);
+
+                    // check for error response
+                    if (!response.ok) {
+                        // get error message from body or default to response status
+                        const error = (data && data.message) || response.status;
+                        return Promise.reject(error);
+                    } else {
+                        reset(data.data);
+                        console.log(data.data);
+                        var msg = props.adding ? 'Added Successfully...!' : 'Updated Successfully..'
+                        toast.success(msg, {
+                            position: "top-right",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+
+                        });
+                    }
+
+
+                    if (response === '200') {
+                    }
+
+                })
+                .catch(error => {
+                    console.error('There was an error!', error);
+                });
+        }
+
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    useEffect(() => {
+
+
+        if (props.adding) {
+            reset({ copay_strategy_id: '',pricing_strategy_name:'',pharm_type_variation_ind:'',network_part_variation_ind:'',claim_type_variation_ind:'',plan_accum_deduct_id:'',formulary_variation_ind:'',price_schedule:'',mac_list:'', new: 1 }, {
+                keepValues: false,
+            })
+        } else {
+            reset(props.selected);
+        }
+
+        if (!props.selected) {
+            reset({ copay_strategy_id: '',pricing_strategy_name:'',pharm_type_variation_ind:'',network_part_variation_ind:'',claim_type_variation_ind:'',plan_accum_deduct_id:'',formulary_variation_ind:'',price_schedule:'',mac_list:'', new: 1 }, {
+                keepValues: false,
+            })
+        }
+
+
+    }, [props.selected, props.adding]);
+
+
+    useEffect(() => { reset(props.selected) }, [props.selected]);
     return (
         <>
-            <div className="card mt-3 mb-3">
+        <form onSubmit={handleSubmit(addCode)} >
+        <div className="card mt-3 mb-3">
                 <div className="card-body">
                     <div className="col-md-12">
                         <h5 className="mb-2">Copay Strategy Identification</h5>
@@ -359,13 +484,21 @@ function CopayStrategyForm(props) {
                         <div className="col-md-6 mb-3">
                             <div className="form-group">
                                 <small> Strategy ID: </small>
-                                <input type="text" name="copay_strategy_id" {...register('copay_strategy_id')} placeholder="" className="form-control" />
+                                <input type="text" name="copay_strategy_id" {...register('copay_strategy_id',{
+                                    required:true,
+                                })}  className="form-control" />
+                           {errors.copay_strategy_id && <span><p role="alert" className="notvalid">This field is required</p></span>}
+
                             </div>
                         </div>
                         <div className="col-md-6 mb-3">
                             <div className="form-group">
                                 <small> Strategy Name: </small>
-                                <input type="text" name="copay_strategy_name" {...register('copay_strategy_name')} placeholder="100PC" className="form-control" />
+                                <input type="text" name="copay_strategy_name" {...register('copay_strategy_name',{
+                                    required:true,
+                                })}  className="form-control" />
+                             {errors.copay_strategy_name && <span><p role="alert" className="notvalid">This field is required</p></span>}
+
                             </div>
                         </div>
                     </div>
@@ -378,46 +511,66 @@ function CopayStrategyForm(props) {
                         <div className="col-md-3 mb-3">
                             <div className="form-group">
                                 <small>Provider Type:</small>
-                                <select className="form-select" name="pharm_type_variation_ind" {...register('pharm_type_variation_ind')}>
+                                <select className="form-select" name="pharm_type_variation_ind" {...register('pharm_type_variation_ind',{
+                                    required:true,
+                                })}>
+                                    <option value="">--select--</option>
                                     <option value="">--select--</option>
                                     <option value="R">Retail</option>
                                     <option value="M">Mail Service </option>
                                     <option value="*">WildCard - No Variation</option>
                                 </select>
+                                {errors.pharm_type_variation_ind && <span><p role="alert" className="notvalid">This field is required</p></span>}
+
                             </div>
                         </div>
                         <div className="col-md-3 mb-3">
                             <div className="form-group">
                                 <small>Network Partification:</small>
-                                <select className="form-select" name="network_part_variation_ind" {...register('network_part_variation_ind')}>
+                                <select className="form-select" name="network_part_variation_ind" {...register('network_part_variation_ind',{
+                                    required:true
+                                })}>
+                                   <option value="">--select--</option>
                                     <option value="I">In Network</option>
                                     <option value="O">Out of Network </option>
                                     <option value="*">WildCard - No Variation</option>
                                 </select>
+                                {errors.network_part_variation_ind && <span><p role="alert" className="notvalid">This field is required</p></span>}
+
 
                             </div>
                         </div>
                         <div className="col-md-3 mb-3">
                             <div className="form-group">
                                 <small>Claim Type:</small>
-                                <select className="form-select" name="claim_type_variation_ind" {...register('claim_type_variation_ind')}>
+                                <select className="form-select" name="claim_type_variation_ind" {...register('claim_type_variation_ind',{
+                                    required:true,
+                                })}>
+                                    <option value="">--select--</option>
                                     <option value="P">POS</option>
                                     <option value="D">DMR </option>
                                     <option value="U">UCF</option>
                                     <option value="*">WildCard - No Variation</option>
                                 </select>
+                                {errors.claim_type_variation_ind && <span><p role="alert" className="notvalid">This field is required</p></span>}
+
 
                             </div>
                         </div>
                         <div className="col-md-3 mb-3">
                             <div className="form-group">
                                 <small>Formulary:</small>
-                                <select className="form-select" name="formulary_variation_ind" {...register('formulary_variation_ind')}>
+                                <select className="form-select" name="formulary_variation_ind" {...register('formulary_variation_ind',{
+                                    required:true,
+                                })}>
+                                   <option value="">--select--</option>
                                     <option value="F">Formularly</option>
                                     <option value="N">Non-Formulary </option>
                                     {/* <option>UCF</option> */}
                                     <option value="*">Wild Card - No Variation</option>
                                 </select>
+                                {errors.formulary_variation_ind && <span><p role="alert" className="notvalid">This field is required</p></span>}
+
 
                             </div>
                         </div>
@@ -438,20 +591,8 @@ function CopayStrategyForm(props) {
                             {/* <a href="" className="btn btn-theme pt-2 pb-2" style={{width: "100%"}}>Next</a> */}
                         </div>
                     </div>
-                </div>
-            </div>
 
 
-
-        </>
-    )
-}
-
-function CopayStrategyIdentifiers() {
-    return (
-        <>
-            <div className="card mt-3 mb-3">
-                <div className="card-body">
 
                     <div className="row mb-2">
                         <div className="col-md-12">
@@ -470,6 +611,15 @@ function CopayStrategyIdentifiers() {
 
                 </div>
             </div>
+            <Button type='submit' variant="primary">{props.adding ? ' Add' : 'Update'}</Button>
+
+        </form>
+
+         
+
+
+
         </>
     )
 }
+
